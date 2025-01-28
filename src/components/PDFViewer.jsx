d001a89f46b4946425f98e3c12f634049667d637
Phyopma <phyopyaemaung1@gmail.com>
2025-01-27 17:34:55 -0800

    fixed rendered

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -3,7 +3,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 const PDFViewer = ({
   file,
